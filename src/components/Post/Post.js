836525b7f2baf102233fbea31b527ad5dedbbe35
JaHe <kubahermyt@gmail.com>
2020-10-01 14:06:20 +0200

    feat: adding clarity for query outside component

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,18 +1,20 @@
 import React from "react"
 import { graphql, useStaticQuery } from "gatsby"
 
-const Post = () => {
-  const data = useStaticQuery(graphql`
-    {
-      site {
-        siteMetadata {
-          title
-          description
-          author
-        }
+const query = graphql`
+  {
+    site {
+      siteMetadata {
+        title
+        description
+        author
       }
     }
-  `)
+  }
+`
+
+const Post = () => {
+  const data = useStaticQuery(query)
 
   const { author, title, description } = data.site.siteMetadata
 
