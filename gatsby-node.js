de93aabef860c2d309e26081cd5d2e2360fe1707
JaHe <kubahermyt@gmail.com>
2020-10-06 08:06:15 +0200

    feat: gatsby-node createPage method

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,57 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require(`path`)
 
-// You can delete this file if you're not using it
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const blogPostTemplate = path.resolve(`src/layouts/post.js`)
+  // Query for markdown nodes to use in creating pages.
+  // You can query for whatever data you want to create pages for e.g.
+  // products, portfolio items, landing pages, etc.
+  // Variables can be added as the second function parameter
+  const result = await graphql(
+    `
+      query queryArticles {
+        allMdx {
+          edges {
+            node {
+              id
+              excerpt(pruneLength: 20)
+              frontmatter {
+                title
+                author
+                slug
+                featuredImage {
+                  childImageSharp {
+                    fluid(maxHeight: 500, maxWidth: 700) {
+                      src
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    `
+  )
+
+  console.log(result.data.allMdx.edges)
+
+  // Create blog post pages.
+  result.data.allMdx.edges.forEach(post => {
+    createPage({
+      // Path for this page — require
+      path: `${post.node.frontmatter.slug}`,
+      component: blogPostTemplate,
+      context: {
+        // Add optional context data to be inserted
+        // as props into the page component..
+        //
+        // The context data can also be used as
+        // arguments to the page GraphQL query.
+        //
+        // The page "path" is always available as a GraphQL
+        // argument.
+      },
+    })
+  })
+}
